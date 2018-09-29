524ee209b8cea58b84e8d993c3414c2aa58e706b
Lina Nielsen <nie15031@byui.edu>
2018-09-29 14:00:46 -0400

    added path to prove02.php

--- web/week02/Prove02/prove02.php
+++ web/week02/Prove02/prove02.php
@@ -9,15 +9,34 @@
     <div class="dropdown-content">
       <a href="#">Week01</a>
       <a href="../ta.html">Week02</a>
-      <a href="#">Week03</a>
+      <a href="#">Week04</a>
+      <a href="#">Week05</a>
+      <a href="#">Week06</a>
+      <a href="#">Week07</a>
+      <a href="#">Week08</a>
+      <a href="#">Week09</a>
+      <a href="#">Week10</a>
+      <a href="#">Week11</a>
+      <a href="#">Week11</a>
+      <a href="#">Week12</a>
     </div>
   </li>
   <li class="dropdown">
     <a href="javascript:void(0)" class="dropbtn">Prove</a>
     <div class="dropdown-content">
       <a href="#">Week01</a>
-      <a href="#">Week02</a>
+      <a href="Prove02.php">Week02</a>
       <a href="#">Week03</a>
+      <a href="#">Week04</a>
+      <a href="#">Week05</a>
+      <a href="#">Week06</a>
+      <a href="#">Week07</a>
+      <a href="#">Week08</a>
+      <a href="#">Week09</a>
+      <a href="#">Week10</a>
+      <a href="#">Week11</a>
+      <a href="#">Week11</a>
+      <a href="#">Week12</a>
     </div>
   </li>
 </ul>

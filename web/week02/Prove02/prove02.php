8148067ed73c7ab279567a3af323a386829ddc8a
Lina Nielsen <nie15031@byui.edu>
2018-09-29 16:14:58 -0400

    modified prove02.php

--- web/week02/Prove02/prove02.php
+++ web/week02/Prove02/prove02.php
@@ -3,7 +3,7 @@
 ?>
 <ul class="navbar">
   <li><a href="prove02.php">Home</a></li>
-  <li><a href="assignments.php">All Assignments</a></li>
+  <li><a href="assignments.php">Assignments</a></li>
   <li class="dropdown">
     <a href="javascript:void(0)" class="dropbtn">Team Activities</a>
     <div class="dropdown-content">

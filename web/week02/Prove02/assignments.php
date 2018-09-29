9ea070741a94674c6eb22cd0488fb3a735c06c44
Lina Nielsen <nie15031@byui.edu>
2018-09-29 16:18:37 -0400

    modified assignments.php to include navbar.php

--- web/week02/Prove02/assignments.php
+++ web/week02/Prove02/assignments.php
@@ -1,45 +1,7 @@
 <?php 
   include('includes/header.php');
+  include('includes/navbar.php');
 ?>
-<ul class="navbar">
-  <li><a href="prove02.php">Home</a></li>
-  <li><a href="assignments.php">All Assignments</a></li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Team Activities</a>
-    <div class="dropdown-content">
-      <a href="#">Week01</a>
-      <a href="../ta.html">Week02</a>
-      <a href="#">Week04</a>
-      <a href="#">Week05</a>
-      <a href="#">Week06</a>
-      <a href="#">Week07</a>
-      <a href="#">Week08</a>
-      <a href="#">Week09</a>
-      <a href="#">Week10</a>
-      <a href="#">Week11</a>
-      <a href="#">Week11</a>
-      <a href="#">Week12</a>
-    </div>
-  </li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Prove</a>
-    <div class="dropdown-content">
-      <a href="#">Week01</a>
-      <a href="prove02.php">Week02</a>
-      <a href="#">Week03</a>
-      <a href="#">Week04</a>
-      <a href="#">Week05</a>
-      <a href="#">Week06</a>
-      <a href="#">Week07</a>
-      <a href="#">Week08</a>
-      <a href="#">Week09</a>
-      <a href="#">Week10</a>
-      <a href="#">Week11</a>
-      <a href="#">Week11</a>
-      <a href="#">Week12</a>
-    </div>
-  </li>
-</ul>
 <main>
   <h1>CS 313: Assignments</h1>
   <dl>

e6e210a8508331f007fcb2cbc65e5b3ba4a8e815
Lina Nielsen <nie15031@byui.edu>
2018-10-05 13:26:29 -0400

    Modified main page to include teach03

--- web/week02/Prove02/includes/navbar.php
+++ web/week02/Prove02/includes/navbar.php
@@ -6,7 +6,7 @@
     <div class="dropdown-content">
       <a href="#">Week01</a>
       <a href="../ta.html">Week02</a>
-      <a href=".../week03/teach03/index.php">Week03</a>
+      <a href="../../week03/teach03/index.php">Week03</a>
       <a href="#">Week04</a>
       <a href="#">Week05</a>
       <a href="#">Week06</a>

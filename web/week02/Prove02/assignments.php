abb7875c87d755ca7c4e357edd81d4255c60ec07
Lina Nielsen <nie15031@byui.edu>
2018-10-08 16:17:09 -0400

    Modified name in assignments.php for some files

--- web/week02/Prove02/assignments.php
+++ web/week02/Prove02/assignments.php
@@ -11,8 +11,8 @@
   <dd>- <a href="../ta.html">Teach02</a></dd>
   <dd>- <a href="prove02.php">Prove02</a></dd>
   <dt>Week03</dt>
-  <dd>- <a href="../../week03/teach03/index.php">Teach02</a></dd>
-  <dd>- <a href="../../week03/prove03/index.php">Prove02</a></dd>
+  <dd>- <a href="../../week03/teach03/index.php">Teach03</a></dd>
+  <dd>- <a href="../../week03/prove03/index.php">Prove03</a></dd>
   <dt>Week04</dt>
   <dd>- Coming Soon</dd>
 </dl>

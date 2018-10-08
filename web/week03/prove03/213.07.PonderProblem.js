36db4326a11434c1069b157aba065be49af5a3d0
Lina Nielsen <nie15031@byui.edu>
2018-10-08 17:01:56 -0400

    Cleaned up some code in .js and .css files

--- web/week03/prove03/213.07.PonderProblem.js
+++ web/week03/prove03/213.07.PonderProblem.js
@@ -60,30 +60,9 @@ function validatePhone(theNumber, theClass) {
     }
 }
 
-function validateCardNumber(theCard, theClass) {
-    var RegExTest = /^\s*[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s[0-9]{4}\s*$/;
-    var RegExTest2 = /^\s*[0-9]{16}\s*$/;
-    if (theCard.match(RegExTest) || theCard.match(RegExTest2)) {
-        document.getElementsByClassName(theClass)[0].style.visibility = "hidden";
-    }
-    else {
-        document.getElementsByClassName(theClass)[0].style.visibility = "visible";
-    }
-}
-
 function validateStAddress(value, theClass) {
     var RegExTest = /^\s*[0-9]+\s[a-zA-Z]+\s[a-zA-Z]+\s*$/;
     if (value.match(RegExTest)) {
-         document.getElementsByClassName(theClass)[0].style.visibility = "hidden";
-    }
-    else {
-        document.getElementsByClassName(theClass)[0].style.visibility = "visible";
-    }
-}
-
-function validateDate(theDate, theClass) {
-    var RegExTest = /^\s*(0?[1-9]|1[0-2])\/\b(201[8-9]|202[0-9]|2030)\s*$/;
-    if (theDate.match(RegExTest)) {
          document.getElementsByClassName(theClass)[0].style.visibility = "hidden";
     }
     else {
@@ -105,43 +84,3 @@ function focusOnForm() {
     document.form.first.focus();
     update('tableItems', 'tableTotalPrice');
 }
-
-function update (tableIt, tablePr) {
-    var numItems = document.getElementById(tableIt).rows.length;
-    var rowsTotal =  document.getElementById(tablePr).rows.length;
-    for (var i = 1; i < numItems; i++) {
-        document.getElementById(tableIt).rows[i].cells[4].innerHTML = "0.00";
-    }
-    for (var e = 0; e < rowsTotal; e++) {
-        document.getElementById(tablePr).rows[e].cells[1].innerHTML = "0.00";
-    }
-}
-
-//function submitForm() {
-//    alert("The form was submitted");
-//}
-
-function sumAmount(number, price, totalprice, tableIt) {
-    var value1 = Number(document.getElementById(price).innerHTML);
-    var total = value1 * Number(Math.round(number));
-    document.getElementById(totalprice).innerHTML = total.toFixed(2);
-    grandtotal(tableIt);
-
-}
-
-function grandtotal(tableIt) {
-    var numItems = document.getElementById(tableIt).rows.length;
-    var subtotal = 0;
-    for (var i = 1; i < numItems; i++) {
-        subtotal += Number(document.getElementById(tableIt).rows[i].cells[4].innerHTML);
-    }
-
-    var tax = Number((subtotal * 0.06).toFixed(2));
-    var shipping = Number(6.99);
-    var grandtotal = subtotal + tax + shipping;
-    document.getElementById("subtotal-price").innerHTML = subtotal.toFixed(2);
-    document.getElementById("tax").innerHTML = tax.toFixed(2);
-    document.getElementById("shipping").innerHTML = shipping;
-    document.getElementById("grandtotal").innerHTML = grandtotal.toFixed(2);
-}
-

f0a0e4b5867a5e45be4adf9d797f629441cbc35a
Lina Nielsen <nie15031@byui.edu>
2018-10-12 21:57:23 -0400

    Modified primary keys in some of the tables

--- db/myDb.sql
+++ db/myDb.sql
@@ -11,18 +11,13 @@ amount money NOT NULL
 CREATE TABLE detail (
 detail_id serial NOT NULL primary key,
 store_name varchar(120) NOT NULL,
-category_id varchar(120) NOT NULL references budget(category_id)
+category_id serial NOT NULL references budget(category_id)
 );
 
 CREATE TABLE expense (
 expense_id serial NOT NULL primary key,
-detail_id varchar(120) NOT NULL references detail(detail_id),
+detail_id serial NOT NULL references detail(detail_id),
 transaction_amount money NOT NULL,
 purchase_date date NOT NULL
 );
 
-/*
-INSERT INTO budget(budget_category, amount) VALUES('grocery','300.00');
-INSERT INTO budget(budget_category, amount) VALUES('electric_bill','200.00');
-INSERT INTO budget(budget_category, amount) VALUES('internet','75.00');
-*/

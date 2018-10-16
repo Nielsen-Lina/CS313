60cfa4b04e0722ec8bd9fe5695001e7ea97180f3
Lina Nielsen <nie15031@byui.edu>
2018-10-16 11:59:54 -0400

    Trying to issue queries

--- web/index.php
+++ web/index.php
@@ -38,7 +38,7 @@ while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 }
 
 // didn't produce any output
-$statement = $db->query('SELECT username, password FROM note_user');
+$statement = $db->prepare('SELECT username, password FROM note_user');
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
  if($result = $statement->fetchAll(PDO::FETCH_ASSOC)){

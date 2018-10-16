a87377e6cef9123a00826d1740a2b04a295c7204
Lina Nielsen <nie15031@byui.edu>
2018-10-16 12:11:59 -0400

    Trying to issue queries

--- web/index.php
+++ web/index.php
@@ -56,4 +56,8 @@ $stmt->bindValue(':name', $name, PDO::PARAM_STR);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->execute(array(':name' => $name, ':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>

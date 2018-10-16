776a1380796f2504a48538e36473350198d3d46d
Lina Nielsen <nie15031@byui.edu>
2018-10-15 20:36:46 -0400

    Trying to issue queries

--- web/index.php
+++ web/index.php
@@ -31,4 +31,13 @@ foreach ($db->query('SELECT username, password FROM note_user') as $row)
   echo '<br/>';
 }
 
+$statement = $db->query('SELECT username, password FROM note_user');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'user: ' . $row['username'] . ' password: ' . $row['password'] . '<br/>';
+}
+
+$statement = $db->query('SELECT username, password FROM note_user');
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
+
 ?>

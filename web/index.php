bc521ffe01501e58a2f40d80c1954003592b4e2b
Lina Nielsen <nie15031@byui.edu>
2018-10-16 12:10:05 -0400

    Trying to issue queries

--- web/index.php
+++ web/index.php
@@ -50,4 +50,10 @@ $results = $statement->fetchAll(PDO::FETCH_ASSOC);
         echo "Not found";
  }
 
+$stmt = $db->prepare('SELECT * FROM table WHERE id=:id AND name=:name');
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$stmt->bindValue(':name', $name, PDO::PARAM_STR);
+$stmt->execute();
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
 ?>

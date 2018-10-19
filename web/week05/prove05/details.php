ad684e774c6871f59d9f54ab97bcfdccfa554d42
Lina Nielsen <nie15031@byui.edu>
2018-10-19 17:42:44 -0400

    Form for possible additions to the original tables

--- web/week05/prove05/details.php
+++ web/week05/prove05/details.php
@@ -30,10 +30,9 @@ $stmt = $db->prepare($sql);
 $stmt->execute(array(':category_id' => $id));
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-echo "<h2>More Details: </h2>";
-foreach ($rows as $row) {
-  echo "<b>" . $row['category_name'] . " </b>";
-  echo "<b>" . $row['amount'] . ":</b>";
+echo "<h2>More Details about " . $rows['category_name'] . ":</h2>";
+//foreach ($rows as $row) {
+  echo $rows['category_name'] . " has " . $rows['amount'] . "allowance.";
   echo '<br/>';
 }
 

2554226bc8018f54a1a687c2149941de411a0b40
Lina Nielsen <nie15031@byui.edu>
2018-10-19 14:44:31 -0400

    Quering information for prove05

---
+++ web/week05/prove05/details.php
@@ -0,0 +1,40 @@
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+} 
+
+
+$id = htmlspecialchars($_GET['id']);
+$sql = 'SELECT category_name, amount FROM budget WHERE id=:id';
+
+$stmt = $db->prepare($sql);
+$stmt->execute(array(':id' => $id));
+$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+echo "<h2>Search Results</h2>";
+foreach ($rows as $row) {
+  echo "<b>" . $row['category_name'] . " </b>";
+  echo "<b>" . $row['amount'] . ":</b>";
+  echo '<br/>';
+}
+
+?>

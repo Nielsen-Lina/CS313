4758d53d8097edd7baa81a1f6e5554a3291a5e84
Lina Nielsen <nie15031@byui.edu>
2018-10-19 17:01:59 -0400

    Quering information for prove05

--- web/week05/prove05/index.php
+++ web/week05/prove05/index.php
@@ -59,14 +59,14 @@ $id = $stmt->fetch(PDO::FETCH_ASSOC);
 $id = $id['category_id'];
 //print_r($id);
 
-//$sql_2 = 'SELECT category_id, company_name FROM detail WHERE category_id=:category_id';
-$sql_2 = 'SELECT detail.company_name FROM budget JOIN detail ON budget.category_id=detail.category_id';
-$stmt = $db->query($sql_2);
-$names = $stmt;
-//$statement = $db->prepare($sql_2);
-//$statement->bindParam(':category_id', $id);
-//$statement->execute();
-//$names = $statement->fetchAll(PDO::FETCH_ASSOC);
+$sql_2 = 'SELECT category_id, company_name FROM detail WHERE category_id=:category_id';
+//$sql_2 = 'SELECT detail.company_name FROM budget JOIN detail ON budget.category_id=detail.category_id';
+//$stmt = $db->query($sql_2);
+//$names = $stmt;
+$statement = $db->prepare($sql_2);
+$statement->bindParam(':category_id', $id);
+$statement->execute();
+$names = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 echo "<ul>";
 foreach ($names as $name)

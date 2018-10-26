3bb75fe207e32984e78fab797d70364fc50af2f2
Lina Nielsen <nie15031@byui.edu>
2018-10-26 16:37:03 -0400

    Adding new page to delete from Budget table

--- web/week05/prove05/delete_budget.php
+++ web/week05/prove05/delete_budget.php
@@ -31,8 +31,10 @@ foreach ($category_chk as $category)
 {
   echo $category;
 
-  $stmtTopics = $db->prepare('DELETE FROM budget WHERE category_name=$category');
+  $stmtTopics = $db->prepare('DELETE FROM budget WHERE category_name=:category');
+  $stmt->bindValue(':category', $category);
   $stmtTopics->execute();
+  
   /*
   $stmtTopics = $db->prepare('UPDATE budget SET category_name=:category_name, amount=:amount WHERE category_name=$category');
   $stmtTopics->bindValue(':category_name', $category_name);

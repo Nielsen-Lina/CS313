f1d6993758da3999ba8c9cb7dd40d06ab59d3528
Lina Nielsen <nie15031@byui.edu>
2018-10-26 12:58:46 -0400

    Adding new page to insert into Budget table

--- web/week05/prove05/changeBudget.php
+++ web/week05/prove05/changeBudget.php
@@ -28,4 +28,14 @@ $amount = htmlspecialchars($_POST['amount']);
 echo $category_name;
 echo $amount;
 
+$stmt = $db->prepare('INSERT INTO budget(category_name, amount) VALUES (:category_name, :amount)');
+$stmt->binValues(':category_name', $category_name, PDO::PARAM_STR);
+$stmt->binValues(':amount', $amount, PDO::PARAM_INT);
+$stmt->execute();
+
+$new_page = "index.php";
+
+header("Location: $new_page");
+die();
+
 ?>

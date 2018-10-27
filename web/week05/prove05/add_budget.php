aea6cf8d1bea286da306ad96c39c62ccce58dd39
Lina Nielsen <nie15031@byui.edu>
2018-10-27 15:02:44 -0400

    Adding error page

--- web/week05/prove05/add_budget.php
+++ web/week05/prove05/add_budget.php
@@ -25,20 +25,27 @@ catch (PDOException $ex)
 $category_name = htmlspecialchars($_POST['category_name']);
 $amount = htmlspecialchars($_POST['amount']);
 
-$res = $db->query('SELECT count(*) FROM budget');
-$category_id = $res->fetchColumn();
+if (!isset($category_name) || trim($category_name) == '')
+{
+  include('error.php');
+}
+else
+{
+  $res = $db->query('SELECT count(*) FROM budget');
+  $category_id = $res->fetchColumn();
 
-(int)($category_id += 1);
+  (int)($category_id += 1);
 
-$stmt = $db->prepare('INSERT INTO budget(category_id, category_name, amount) 
-	VALUES (:category_id, :category_name, :amount)');
-$stmt->bindValue(':category_id', $category_id, PDO::PARAM_INT);
-$stmt->bindValue(':category_name', ucfirst($category_name), PDO::PARAM_STR);
-$stmt->bindValue(':amount', $amount, PDO::PARAM_INT);
-$stmt->execute();
+  $stmt = $db->prepare('INSERT INTO budget(category_id, category_name, amount) 
+  	VALUES (:category_id, :category_name, :amount)');
+  $stmt->bindValue(':category_id', $category_id, PDO::PARAM_INT);
+  $stmt->bindValue(':category_name', ucfirst($category_name), PDO::PARAM_STR);
+  $stmt->bindValue(':amount', $amount, PDO::PARAM_INT);
+  $stmt->execute();
 
-$new_page = "change.php";
-header("Location: $new_page");
-die();
+  $new_page = "change.php";
+  header("Location: $new_page");
+  die();
+}
 
 ?>

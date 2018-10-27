e8611739994b342d9e7a5c387b6b285aaad505f6
Lina Nielsen <nie15031@byui.edu>
2018-10-27 00:43:44 -0400

    Working on css

--- web/week05/prove05/includes/header.php
+++ web/week05/prove05/includes/header.php
@@ -2,9 +2,8 @@
 <html>
    <head>
         <title>Expense Management System</title>
-        <link href="prove02style.css" rel="stylesheet" media="screen" type="text/css"/>
+        <link href="style.css" rel="stylesheet" media="screen" type="text/css"/>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript" src="prove02script.js"></script>
     </head>
     <body>
         <!-- Header -->

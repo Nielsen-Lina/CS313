19590159ec81888ca20142c361429de04a10fb4f
Lina Nielsen <nie15031@byui.edu>
2018-10-27 01:26:17 -0400

    Working on css

--- web/week05/prove05/includes/navbar.php
+++ web/week05/prove05/includes/navbar.php
@@ -1,10 +1,6 @@
 <ul class="navbar">
   <li><a href="./index.php">Home</a></li>
   <li><a href="./change.php">Make Changes</a></li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Changes</a>
-    <div class="dropdown-content">
-      <a href="change.php">C</a>
     </div>
   </li>
 </ul>

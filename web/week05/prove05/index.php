e7aa2f65c1e211ada5e97ef3c5384a7761dafd70
Lina Nielsen <nie15031@byui.edu>
2018-10-26 23:32:26 -0400

    Adding new page to update Expense table

--- web/week05/prove05/index.php
+++ web/week05/prove05/index.php
@@ -84,6 +84,12 @@ $details = $stmtDetail->fetchAll(PDO::FETCH_ASSOC);
 
 $company_chk = [];
 
+$stmtExpense = $db->prepare('SELECT expense.expense_id, detail.company_name, expense.transaction_amount, expense.purchase_date FROM detail JOIN expense ON detail.detail_id=expense.detail_id');
+$stmtExpense->execute(array());
+$expenses = $stmtExpense->fetchAll(PDO::FETCH_ASSOC);
+
+$expense_chk = [];
+
 ?>
 
 <br/>
@@ -107,7 +113,7 @@ $company_chk = [];
   <input type="text" name="category_name" placeholder="category name"><br>
   <input type="submit" value="Add"><br>
   <p>Select a company name in order to do the following:</p><br>
-  <input type="submit" name="update_company" formaction="update_detail.php" value="Update Comapny"><br>
+  <input type="submit" name="update_company" formaction="update_detail.php" value="Update Company"><br>
   <input type="submit" name="update_category" formaction="update_detail.php" value="Update Category"><br>
   <input type="submit" formaction="delete_detail.php" value="Delete"><br>
   <?php foreach ($details as $detail) : ?>
@@ -116,12 +122,19 @@ $company_chk = [];
   <?php endforeach; ?>
 </form>
 <h3>Change a new expense with its accompanied company name, amount and date of purchase:</h3>
-<form method="POST" action="changeExpense.php">
+<form method="POST" action="add_expense.php">
   <input type="text" name="company_name" placeholder="company name">
   <input type="text" name="transaction_amount" placeholder="transaction amount">
-  <input type="text" name="purchase_date" placeholder="purchase date">
-  <input type="submit" value="Add">
-  <input type="submit" value="Update">
-  <input type="submit" value="Delete">
+  <input type="text" name="purchase_date" placeholder="purchase date"><br>
+  <input type="submit" value="Add"><br>
+  <ul>
+  <?php foreach ($expenses as $expense) : ?>
+    <input type="checkbox" name="expense_chk[]" value="<?= $expense['expense_id']; ?>"/><li><?php echo $expense['company_name'] . " " . $expense['transaction_amount'] . " " . $expense['purchase_date'] ?></li><br>
+  <?php endforeach; ?>
+  </ul>
+  <input type="submit" name="update_company_name" formaction="update_budget.php" value="Update Company Name"><br>
+  <input type="submit" name="update_transaction_amount" formaction="update_budget.php" value="Update Transaction Amount"><br>
+  <input type="submit" name="update_date" formaction="update_budget.php" value="Update Purchase Date"><br>
+  <input type="submit" formaction="delete_budget.php" value="Delete"><br>
 </form>
 

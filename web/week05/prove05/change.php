4f1d5647b222dfe9e52250534b2fac4c69ae9227
Lina Nielsen <nie15031@byui.edu>
2018-10-27 22:00:36 -0400

    Created dbConnect.php

--- web/week05/prove05/change.php
+++ web/week05/prove05/change.php
@@ -3,27 +3,8 @@
 include('includes/header.php');
 include('includes/navbar.php');  
 
-try
-{
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
-  $dbPort = $dbOpts["port"];
-  $dbUser = $dbOpts["user"];
-  $dbPassword = $dbOpts["pass"];
-  $dbName = ltrim($dbOpts["path"],'/');
-
-  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-
-  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-} 
+require("dbConnect.php");
+$db = get_db();
 
 $stmt = $db->prepare('SELECT category_id, category_name, amount FROM budget');
 $stmt->execute(array());

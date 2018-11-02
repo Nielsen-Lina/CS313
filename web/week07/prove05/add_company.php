5b25ce4d96959815138e6302a8231912fc150bbd
Lina Nielsen <nie15031@byui.edu>
2018-11-02 14:55:09 -0400

    Checking for incorrect category name input

--- web/week07/prove05/add_company.php
+++ web/week07/prove05/add_company.php
@@ -31,7 +31,8 @@ else
   $category_name_findings = $stmtName->fetchAll(PDO::FETCH_ASSOC);
   foreach ($category_name_findings as $name)
   {
-    echo $name['category_name'];/*
+    echo $name['category_name'];
+    echo ucfirst($category_name) . "<br>";/*
     if ($name['category_name'] == ucfirst($category_name))
     {
         $stmtId = $db->prepare('SELECT category_id FROM budget WHERE category_name=:category_name');

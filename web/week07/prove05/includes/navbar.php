7c757820c847f2d1b3292b80e387116f5f93219a
Lina Nielsen <nie15031@byui.edu>
2018-11-02 14:07:38 -0400

    Modified navbar to include transactions

--- web/week07/prove05/includes/navbar.php
+++ web/week07/prove05/includes/navbar.php
@@ -1,6 +1,7 @@
 <ul class="navbar">
   <li><a href="./login.php">Log In</a></li>
   <li><a href="./index.php">Home</a></li>
+  <li><a href="./transactions.php">Transactions</a></li>
   <li><a href="./change.php">Make Changes</a></li>
   <li><a href="./logout.php">Log Out</a></li>
 </ul>

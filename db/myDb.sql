130e4eb7972c09ee0df0b43b1161ab27484c64b5
Lina Nielsen <nie15031@byui.edu>
2018-11-02 11:03:53 -0400

    Modified myDb.sql

--- db/myDb.sql
+++ db/myDb.sql
@@ -24,3 +24,6 @@ purchase_date date NOT NULL
 INSERT INTO budget VALUES (1, 'Income', -6000), (2, 'Transportation', 800), (3, 'Housing', 1500), (4, 'Savings', 1000), (5, 'Utilities', 400), (6, 'Health Care', 200), (7, 'Consumer Debt', 1500), (8, 'Food and Grocery', 300), (9, 'Entertainment', 300);
 INSERT INTO detail(company_name, category_id) VALUES ('Bosch', 1), ('Chase Car Loan', 2), ('AAA', 2), ('Shell', 2), ('Chase Mortgage', 3), ('BellTire', 2), ('Chase Savings', 4), ('Vanguard', 4), ('Comcast', 5), ('T-Mobile', 5), ('DTE', 5), ('BCBS', 6), ('Chase Credit Card', 7), ('Citi Credit Card', 7), ('Kroger', 8), ('Costco', 8), ('Meijer', 8), ('AMC', 9), ('Netflix', 9), ('Apple Vacation', 9); 
 INSERT INTO expense(detail_id, transaction_amount, purchase_date) VALUES (2, 300,'01 Oct 2018'), (1, -6100, '01 Oct 2018'), (8, 500, '01 Oct 2018'), (7, 500, '01 Oct 2018'), (5, 1500, '03 Oct 2018'), (15, 100, '05 Oct 2018'), (16, 80, '05 Oct 2018');
+
+CREATE USER new_user WITH PASSWORD 'new_pass';
+GRANT SELECT, INSERT, UPDATE, DELETE ON budget AND detail AND expense TO new_user;

516ac8556c0804da3a0629443f907e289a345352
Lina Nielsen <nie15031@byui.edu>
2018-11-02 13:36:43 -0400

    Modifying login file

--- web/week07/prove05/login.php
+++ web/week07/prove05/login.php
@@ -8,7 +8,12 @@ $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
 $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 $error = '';
 $check = false;
-$loggedIn = false;
+
+if ($_SESSION['username'] == 'new_user' && $_SESSION['password'] == 'new_pass')
+{
+    header("Location: index.php");
+    die();
+}
 
 if ($username == 'new_user' && $password == 'new_pass')
 {
@@ -16,7 +21,6 @@ if ($username == 'new_user' && $password == 'new_pass')
     $db = get_db();
     $_SESSION['username'] = 'new_user';
     $_SESSION['password'] = 'new_pass';
-    $loggedIn = true;
     header("Location: index.php");
     die();
 }
@@ -30,25 +34,19 @@ else
     $check = true;
     $error = 'Username and Password do not match! Try again.';
 }
+
 ?>
 
 <html>
 <body>
 <main>
     <form action="" method="post">
-        <?php if (!$loggedIn) : ?>
-            <?php if ($check) : ?>
-                <p class="invalid"><?php echo $error; ?></p>
-            <?php endif; ?>
-            <input type="text" name="username" id="username" placeholder="Username" />
-            <input class="" type="password" name="password" id="password" placeholder="Password" />
-            <input type="submit" value="Login">
-        <?php else : ?>
-            <?php 
-            header("Location: index.php");
-            die(); 
-            ?>
+        <?php if ($check) : ?>
+            <p class="invalid"><?php echo $error; ?></p>
         <?php endif; ?>
+        <input type="text" name="username" id="username" placeholder="Username" />
+        <input class="" type="password" name="password" id="password" placeholder="Password" />
+        <input type="submit" value="Login">
     </form>
 </main>
 </body>
